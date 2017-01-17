e945bdae30d8420ad625e3aa305250cca2aacd7b
retornam <retornam@collectivehealth.com>
2017-01-17 10:00:41 -0800

    update shasum

--- browserstack-local.rb
+++ browserstack-local.rb
@@ -4,7 +4,7 @@ require "formula"
 class BrowserstackLocal < Formula
   homepage "http://www.browserstack.com/local-testing"
   url "https://www.browserstack.com/browserstack-local/BrowserStackLocal-darwin-x64.zip"
-  sha1 "b8b58d1725087ab208fd3464b22c35af790bfcb1"
+  sha256 "0b56df391c112fb9151854561113cc2f19063ff0e66cf2575057c25beb472f18"
 
   def install
     bin.install 'BrowserStackLocal'
